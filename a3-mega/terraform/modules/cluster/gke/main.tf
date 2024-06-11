eb2eb77ecc2a599cb2bfe5c5e14f4337efa269ac
Rick(Rugui) Chen <rickruguichen@google.com>
2024-06-11 17:46:00 -0400

    update reservation

--- a3-mega/terraform/modules/cluster/gke/main.tf
+++ a3-mega/terraform/modules/cluster/gke/main.tf
@@ -197,14 +197,7 @@ resource "google_container_node_pool" "node-pools" {
     # disabling auto_upgrade to stop automatic upgrade during customer workload execution.
     auto_upgrade = false
   }
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28" # Choose a private CIDR block
-    master_global_access_config {
-      enabled = false
-    }
-  }
+  
   node_config {
     service_account = local.node_service_account
     machine_type    = var.node_pools[count.index].machine_type
